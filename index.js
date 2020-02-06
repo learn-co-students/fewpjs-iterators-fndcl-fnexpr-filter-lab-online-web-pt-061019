e72d1e23312f937c032276eb4cec4fd4ce29596a
Ingrid04 <ingridfmelgar@gmail.com>
2020-02-06 16:24:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter( possibleMatch => 
+        possibleMatch.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter( possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0 )
+
+}
+
+function matchName( drivers, string) {
+    return drivers.filter( matchingName => matchingName.name === string)
+}
